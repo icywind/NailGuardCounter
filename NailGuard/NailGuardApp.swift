205b951c004adcc0e1784fae6a6ff574de5001b9
Rick <rcsw.devel@gmail.com>
2026-02-03 10:42:17 -0800

    Initial Commit

---
+++ NailGuard/NailGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NailGuardApp.swift
+//  NailGuard
+//
+//  Created by Rick Cheng on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NailGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NailGuardTests/NailGuardTests.swift
@@ -0,0 +1,17 @@
+//
+//  NailGuardTests.swift
+//  NailGuardTests
+//
+//  Created by Rick Cheng on 2/3/26.
+//
+
+import Testing
+@testable import NailGuard
+
+struct NailGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
